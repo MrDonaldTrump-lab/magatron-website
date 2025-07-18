de5b577e3656a815069bc2d876bef528dd624c48
MrDonaldTrump-lab <testingmethodsandhrsolutions@gmail.com>
2025-07-19 09:11:35 +1000

    Update _app.js

--- magatron-site/pages/_app.js
+++ magatron-site/pages/_app.js
@@ -1,5 +1,15 @@
+import Head from 'next/head';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>MAGATron</title>
+        <link rel="icon" href="/fmagatron-logo.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
